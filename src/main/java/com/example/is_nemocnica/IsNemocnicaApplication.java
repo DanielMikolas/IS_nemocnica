bab8d339b8bd32882205454a1f648256be9266e4
Daniel Mikoláš <danielpc@MacBook-Pro-uzivatela-Daniel-2.local>
2024-11-06 15:41:21 +0100

    Initial commit

---
+++ src/main/java/com/example/is_nemocnica/IsNemocnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.is_nemocnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsNemocnicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsNemocnicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/is_nemocnica/IsNemocnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.is_nemocnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsNemocnicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
